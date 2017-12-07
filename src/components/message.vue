d1f15ec60d10032ddff7669e84b10dc6c0da52d1
= <=>
2017-12-07 00:00:15 -0500

    'first'

---
+++ src/components/message.vue
@@ -0,0 +1,23 @@
+<template>
+  
+  <div class = 'box'>
+    <p>
+      <slot></slot>
+    </p>
+
+
+  </div>
+
+
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  background-color: pink;
+}
+</style>
